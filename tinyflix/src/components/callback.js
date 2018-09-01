52e0b2edfa5ed3553adc457a619e6d637203b152
James <james.randolph2016@gmail.com>
2018-09-01 11:16:58 -0400

    finish component for callbac

--- tinyflix/src/components/callback.js
+++ tinyflix/src/components/callback.js
@@ -2,5 +2,16 @@ import { Component } from 'react';
 import { setIdToken, setAccessToken } from '../utils/authservice';
 
 class Callback extends Component {
-    
+
+    componentDidMount() {
+        setAccessToken();
+        setIdToken();
+        window.location.href = "/";
+    }
+
+    render() {
+        return null;
+    }
 }
+
+export default Callback;
