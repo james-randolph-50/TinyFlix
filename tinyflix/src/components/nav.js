eb5f680b57e80a4e416c024cf5f94e069cc35423
James <james.randolph2016@gmail.com>
2018-09-01 11:20:38 -0400

    nav presnetation component

---
+++ tinyflix/src/components/nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { login, logout, isLoggedIn } from '../utils/authservice';
+import '../App.css';
+
+class Nav extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+        )
+    }
+}
